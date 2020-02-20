1c9f9b21f9ec35880226ecbf255346b2ddb6a763
Ryan Hollins <ballbaroi@gmail.com>
2020-02-20 15:57:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,55 +5,50 @@ end
 def sort_array_desc(integers)
   integers.sort {|first_num, second_num| second_num <=> first_num}
 end
-  
-  def swap_elements (array)
-  array[1], array[2] = array[2], array[1]
-  array
+
+def sort_array_char_count(strings)
+  strings.sort {|left, right| left.length <=> right.length}
 end
 
-
-
-def reverse_array (array)
-  array.reverse
+def swap_elements(strings)
+  strings[1], strings[2] = strings[2], strings[1]
+  return strings
 end
 
-def greater_and_less_than_10 (array)
-  hash = {
-    "greater_than_10" => [],
-    "less_than_10" => []
-  }
-  array.each do |number|
-    if number > 10 
-      hash["greater_than_10"] << number 
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  return array
+end
+
+def reverse_array(integers)
+  new_array = integers.reverse
+  new_array
+end
+
+def kesha_maker(array)
+  kesha = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
+  end
+  kesha
+end
+
+def find_a(array)
+  array.select{|string| string.start_with?("a")}
+end
+
+def sum_array(integers)
+  integers.inject{|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
     else
-      hash["less_than_10"] << number
+      string << "s"
     end
-  end
-  hash
-end
-
-
-
-def find_winners (hash)
-  winners = []
-  hash.each do |name, status| 
-    winners << name if status == "winner"
-  end
-  winners
-end
-
-
-
-def find_a (array)
-  array.select do |word|
-    word.chr == "a"
-  end
-end
-
-
-
-def sum_array (array)
-  array.inject do |sum, num|
-    sum + num
   end
 end
